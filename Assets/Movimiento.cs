cabc799dbcf81563fb07f9601eceb6e1daf814f2
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-09-22 09:43:10 -0500

    inital commit

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour
+{
+    public float velocidadcaidarapida;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 5.0f;
+    public float gravityValue = -9.81f;
+
+    private void Start()
+    {
+         controller = this.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            gravityValue = -9.81f;
+            playerVelocity.y = 0f;
+            velocidadcaidarapida = 1;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (Input.GetButton("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+        if(Input.GetButtonUp("Jump")){
+            velocidadcaidarapida = 2;
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
---
+++ Assets/camara.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+    public Transform Cube;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset=Cube.position-transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Cube != null){
+            transform.position = Cube.position-offset;
+        }
+    }
+}
