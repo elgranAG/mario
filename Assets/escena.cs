7e2ac0332597acd610bf3ff7789c1ad625c6bc00
Sala de Informática 6 <Sala6@academia.usbctg.local>
2023-09-22 18:14:08 -0500

    initial commit

---
+++ Assets/escena.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class escena : MonoBehaviour
+{
+   public void Jugar(){
+     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+   }
+
+   public void salir(){
+    Debug.Log("salir....");
+    Application.Quit();
+   }
+}
